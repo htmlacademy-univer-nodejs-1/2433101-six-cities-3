28cc0502559be80432f677a9ffcb847e6f3f4b2e
Vladislav Maslakov <maslakov20@mail.ru>
2024-03-29 13:04:48 +0500

    schema

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,15 @@
+import { Config, RestSchema } from '../shared/libs/config';
 import { Logger } from '../shared/libs/logger';
 
 
 export class RestApplication {
   constructor (
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config<RestSchema>,
   ) {}
 
   public async init() {
     this.logger.info('Application Initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
