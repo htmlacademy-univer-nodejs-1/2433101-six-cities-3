c2d225389bfe7292789761cba9e0a788023b880a
Vladislav Maslakov <maslakov20@mail.ru>
2024-05-21 15:24:11 +0500

    work 6.1

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,6 +5,9 @@ import { Component } from '../shared/types/component.enum';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
 import express, { Express } from 'express';
+import { OfferController } from '../shared/modules/offer/offer.controller';
+import { ExceptionFilter } from '../shared/libs/rest/exception-filter/exception-filter.interface';
+import { Controller } from '../shared/libs/rest/controller/controller.interface';
 
 
 @injectable()
@@ -15,6 +18,9 @@ export class RestApplication {
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
+    @inject(Component.OfferController) private readonly offerController: OfferController,
+    @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
+    @inject(Component.UserController) private readonly userController: Controller,
   ) {
     this.server = express();
   }
@@ -36,6 +42,19 @@ export class RestApplication {
     this.server.listen(port);
   }
 
+  private async _initControllers() {
+    this.server.use('/users', this.userController.router);
+    this.server.use('/offers', this.offerController.router);
+  }
+
+  private async _initMiddleware() {
+    this.server.use(express.json());
+  }
+
+  private async _initExceptionFilters() {
+    this.server.use(this.appExceptionFilter.catch.bind(this.appExceptionFilter));
+  }
+
   public async init() {
     this.logger.info('Application Initialization');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
@@ -43,6 +62,18 @@ export class RestApplication {
     this.logger.info('Init database…');
     await this._initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init app-level middleware');
+    await this._initMiddleware();
+    this.logger.info('App-level middleware initialization completed');
+
+    this.logger.info('Init controllers');
+    await this._initControllers();
+    this.logger.info('Controller initialization completed');
+
+    this.logger.info('Init exception filters');
+    await this._initExceptionFilters();
+    this.logger.info('Exception filters initialization compleated');
 
     this.logger.info('Trying to init server');
     await this._initServer();
